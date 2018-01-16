ef9e41d471b85c3ff1c6093acbc67a685c7b700b
Mati Halperin <matias.halperin@gmail.com>
2018-01-16 17:38:51 -0300

    msm8998: Rebrand oppo to oneplus

--- common.mk
+++ common.mk
@@ -396,8 +396,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt
 
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
+# Inherit from oneplus-common
+$(call inherit-product, device/oneplus/common/common.mk)
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
